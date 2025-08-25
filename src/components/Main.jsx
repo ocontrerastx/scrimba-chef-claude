5d673c8a0d6d23a5559a95f2afca57f06788c473
Osvaldo Contreras <ocontreras.tx@gmail.com>
2025-08-25 15:32:33 -0500

    Added Main component and included add ingredient form and styling

---
+++ src/components/Main.jsx
@@ -0,0 +1,16 @@
+function Main() {
+  return (
+    <main>
+      <form className="add-ingredient-form">
+        <input
+          type="text"
+          placeholder="e.g. oregano"
+          aria-label="Add ingredient"
+        />
+        <button>Add ingredient</button>
+      </form>
+    </main>
+  );
+}
+
+export default Main;
