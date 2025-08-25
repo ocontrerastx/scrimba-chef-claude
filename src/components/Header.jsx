3064afec83b67b457d3a259793f42e88401088e6
Osvaldo Contreras <ocontreras.tx@gmail.com>
2025-08-25 14:54:56 -0500

    Added header content and styling

---
+++ src/components/Header.jsx
@@ -0,0 +1,12 @@
+import chefClaudeLogo from "../assets/chef-claude-logo.png";
+
+function Header() {
+  return (
+    <header>
+      <img src={chefClaudeLogo} alt="Chef Claude Logo" />
+      <h1>Chef Claude</h1>
+    </header>
+  );
+}
+
+export default Header;
